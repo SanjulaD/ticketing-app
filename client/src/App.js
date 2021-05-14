ee60d2dadd41865309820eff1da7be0b0233f541
SanjulaD <desanjula99@gmail.com>
2021-05-14 19:01:45 +0530

    post service

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { AddPosts } from "./components/posts/AddPosts"
+import ListPosts from "./components/posts/ListPosts"
+
 const App = () => {
   return (
     <div className="container p-3">
-      <h1>Blog Posts</h1>
+      <h1 className="text-center">Blog Posts</h1>
       <div>
         <h2>Create Posts</h2>
       </div>
+      <AddPosts />
+      <h2 className="mt-3 mb-2">List of Posts</h2>
+      <ListPosts />
     </div>
   );
 }
--- client/src/components/posts/addPosts.js
+++ client/src/components/posts/addPosts.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-export const addPosts = () => {
+export const AddPosts = () => {
+
+    const [title, setTitle] = useState("")
+
+    const submitHandler = async (event) => {
+        event.preventDefault()
+
+        await axios.post('http://localhost:4000/posts', {
+            title
+        })
+
+        setTitle("")
+    }
+
     return (
         <div>
-            
+            <form onSubmit={submitHandler}>
+                <div className="form-group p-2">
+                    <label>Title</label>
+                    <input
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        className="form-control"
+                    />
+                    <button className="btn btn-primary mt-2">Submit</button>
+                </div>
+            </form>
         </div>
     )
 }
