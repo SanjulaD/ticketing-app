a0529661fa7f14ef124066af8171901b2b065f90
SanjulaD <desanjula99@gmail.com>
2021-06-09 19:22:55 +0530

    user model with hashing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import 'express-async-errors'
+import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signIn";
@@ -25,10 +26,29 @@ app.all("*", async (req, res) => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongoose://auth-mongo-srv:27017", {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected')
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening port 3000!!");
+  });
+};
+
 app.get("/api/my", (req, res) => {
   res.send("helloo");
 });
 
-app.listen(3000, () => {
-  console.log("Listening port 3000!!");
-});
+start()
+
+
